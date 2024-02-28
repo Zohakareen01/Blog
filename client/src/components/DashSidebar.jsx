8c4c36938cfbd4957dc6e48279ee8a0adb755145
Zoha <zoha.kareen@internee.itskillxpress.com>
2024-02-28 10:42:25 +0500

    complete users image upload functionaliyt

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,34 +1,40 @@
-import { Sidebar } from 'flowbite-react'
-import {HiArrowSmRight, HiUser} from 'react-icons/hi'
-import { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
-import { useLocation } from 'react-router-dom'
-import React from 'react'
+import { Sidebar } from "flowbite-react";
+import { HiArrowSmRight, HiUser } from "react-icons/hi";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+import React from "react";
 
 export default function DashSidebar() {
-    const location = useLocation()
-  const [tab, setTab] = useState('')
-  useEffect(()=>{
-    const urlParams= new URLSearchParams(location.search)
-    const tabFromUrl =  urlParams.get('tab')
-    if(tabFromUrl){
+  const location = useLocation();
+  const [tab, setTab] = useState("");
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get("tab");
+    if (tabFromUrl) {
       setTab(tabFromUrl);
     }
-  }, [location.search])
+  }, [location.search]);
   return (
-   <Sidebar className='w-full md:w-56'>
-    <Sidebar.Items>
-         <Sidebar.ItemGroup>
-            <Link to='/dashboard?tab=profile'>
-            <Sidebar.Item active={tab === 'profile'} icon={HiUser} label ={'User'} labelColor='dark'>
-             Profile
+    <Sidebar className="w-full md:w-56">
+      <Sidebar.Items>
+        <Sidebar.ItemGroup>
+          <Link to="/dashboard?tab=profile">
+            <Sidebar.Item
+              active={tab === "profile"}
+              icon={HiUser}
+              label={"User"}
+              labelColor="dark"
+              as='div'//for fixing this error => validateDOMNesting(...): <a> cannot appear as a descendant of <a>.
+            >
+              Profile
             </Sidebar.Item>
-            </Link>
-            <Sidebar.Item  icon={HiArrowSmRight} className='cursor-pointer'>
+          </Link>
+          <Sidebar.Item icon={HiArrowSmRight} className="cursor-pointer">
             Signout
-            </Sidebar.Item>
-         </Sidebar.ItemGroup>
-    </Sidebar.Items>
-   </Sidebar>
-  )
+          </Sidebar.Item>
+        </Sidebar.ItemGroup>
+      </Sidebar.Items>
+    </Sidebar>
+  );
 }
