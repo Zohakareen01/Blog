e99631486060b3a8149ad8b48d35a74b52ab148e
Zoha <zoha.kareen@internee.itskillxpress.com>
2024-03-01 20:06:20 +0500

    complete

--- client/src/components/CallToAction.jsx
+++ client/src/components/CallToAction.jsx
@@ -8,16 +8,16 @@ export default function CallToAction() {
                 Want to learn more about JavaScript?
             </h2>
             <p className='text-gray-500 my-2'>
-                Checkout these resources with 100 JavaScript Projects
+                Checkout these resources with 100 MERN stack Projects
             </p>
             <Button gradientDuoTone='purpleToPink' className='rounded-tl-xl rounded-bl-none'>
                 <a href="https://www.facebook.com" target='_blank' rel='noopener noreferrer'>
-                    JavaScript Projects
+                   MERN Stack Projects
                 </a>
             </Button>
         </div>
         <div className="p-7 flex-1">
-            <img src="https://bairesdev.mo.cloudinary.net/blog/2023/08/What-Is-JavaScript-Used-For.jpg" />
+            <img src="https://www.rlogical.com/wp-content/uploads/2020/12/MERN.webp" />
         </div>
     </div>
   )
--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -8,7 +8,7 @@ export default function About() {
           </h1>
           <div className='text-md text-gray-500 flex flex-col gap-6'>
             <p>
-              Welcome to Zoha's Blog! This blog was created by zoha Ghavidel
+              Welcome to Zoha's Blog! This blog was created by zoha 
               as a personal project to share his thoughts and ideas with the
               world. zoha is a passionate developer who loves to write about
               technology, coding, and everything in between.
